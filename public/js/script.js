68fadafa8dc875758e632231e99b2b87200080b1
Vasili Zolotov <vasili@leopard-1.local>
2011-11-15 23:06:32 -0800

    better edge detection, better distance function

--- public/js/script.js
+++ public/js/script.js
@@ -4,8 +4,10 @@ Instragram challenge unshredder
 */
 
 var step = 32;
-var slices = [];
-var edges = [];
+var slices;
+var edges;
+
+var debug = {};
 
 function loadImage( path ) {
     var srcImage = new Image();
@@ -18,7 +20,7 @@ function processImage( img ) {
     
     var canvas = document.getElementById("source");
     var context = canvas.getContext("2d");
-    context.drawImage(img, 0, 0); 
+    context.drawImage( img, 0, 0 ); 
     
     for ( var n=0; n * step < img.width; n++ ) {
         slices[n] = context.getImageData( n*step, 0, step, img.height );
@@ -27,29 +29,49 @@ function processImage( img ) {
     for ( var l=0; l < slices.length; l++ ) {
         for ( var r=0; r < slices.length; r++ ) if ( l != r ) {
             var d = distance( slices[l], slices[r] );
-            if ( edges[l] == null || d > edges[l][1] ) {
-                edges[l] = [ r, d ];
-            }
+            if ( edges[l] == null || d < edges[l][1] ) edges[l] = [ r, d ];
         }
     }
     
-    for ( var n=0; n<slices.length; n++ ) {
-        if ( edges.rightmost == undefined || edges[edges.rightmost][1] > edges[n][1] ) edges.rightmost = n;
+    var dmax = 0;
+    for ( var n=0; n < edges.length; n++ ) {
+        if ( edges[n][1] > edges[dmax][1] ) dmax = n;
     }
     
-    var slice = edges.rightmost;
-    for ( var slot = 0; slot < slices.length; slot++ ) {
-        drawSlice( slice, slices.length -1 - slot );
-        for ( var i = 0; i < edges.length; i++ ) if ( slice == edges[i][0] && i != edges.rightmost ) { slice = i; break; }
+    var index = {}
+    for ( var n=0; n < edges.length; n++ ) {
+        if ( index[edges[n][0]] == undefined || edges[n][1] < index[edges[n][0]][1] ) index[edges[n][0]] = [ n, edges[n][1] ];
     }
+    
+    var left = dmax;
+    for ( var n=0; n < edges.length; n++ ) {
+        if ( index[left] == undefined || index[left] == dmax ) break;
+        left = index[left][0];
+    }
+    
+    var slice = left;
+    var dsum = 0;
+    for ( var slot = 0; slot < slices.length; slot++ ) {
+        drawSlice( slice, slot );
+        dsum += edges[slice][1];
+        slice = edges[slice][0];
+        
+    }
+    
+    
+    debug.index = index;
+    debug.dmax = dmax;
+    debug.left = left;
+    debug.dsum = dsum;
+    debug.dnorm = dsum / slices.length;
 }
 
 function distance( left, right ) {
     var sum = 0;
-    for ( var y=0; y < left.height-1; y++ ) {
+    for ( var y=1; y < left.height-1; y++ ) {
         var d = 0;
-        for ( var c=0; c<4; c++ ) { d+= Math.pow( left.data[step*y*4+(step-1)*4+c]-right.data[step*y*4+c], 2 ) }
-        sum += 1/(Math.sqrt( d )+1);
+        for ( var c=0; c<4; c++ ) { d+= Math.pow( left.data[step*y*4+(step-1)*4+c]-right.data[step*y*4+c] , 2 ) }
+        sum += 1 - 1/(Math.sqrt(d+1));
      }
     return sum;
 }
