d37c27a9ae89580c48ee740a2a3b080440956040
Vasili Zolotov <vasili@leopard-1.local>
2011-11-16 13:01:50 -0800

    comments

--- public/js/script.js
+++ public/js/script.js
@@ -21,34 +21,34 @@ function processImage( img ) {
     var canvas = document.getElementById("source");
     var context = canvas.getContext("2d");
     context.drawImage( img, 0, 0 ); 
-    
+    // produce slices
     for ( var n=0; n * step < img.width; n++ ) {
         slices[n] = context.getImageData( n*step, 0, step, img.height );
     }
-    
+    // find shortest right edge for each tile
     for ( var l=0; l < slices.length; l++ ) {
         for ( var r=0; r < slices.length; r++ ) if ( l != r ) {
             var d = distance( slices[l], slices[r] );
             if ( edges[l] == null || d < edges[l][1] ) edges[l] = [ r, d ];
         }
     }
-    
+    // find longest edge among the shortest
     var dmax = 0;
     for ( var n=0; n < edges.length; n++ ) {
         if ( edges[n][1] > edges[dmax][1] ) dmax = n;
     }
-    
+    // build an index on edge target to traverse edges right to left
     var index = {}
     for ( var n=0; n < edges.length; n++ ) {
         if ( index[edges[n][0]] == undefined || edges[n][1] < index[edges[n][0]][1] ) index[edges[n][0]] = [ n, edges[n][1] ];
     }
-    
+    // find the leftmost tile
     var left = dmax;
     for ( var n=0; n < edges.length; n++ ) {
         if ( index[left] == undefined || index[left] == dmax ) break;
         left = index[left][0];
     }
-    
+    // draw the tiles left to right
     var slice = left;
     var dsum = 0;
     for ( var slot = 0; slot < slices.length; slot++ ) {
@@ -57,7 +57,7 @@ function processImage( img ) {
         slice = edges[slice][0];
         
     }
-    
+    // dump state for debugging
     debug.index = index;
     debug.dmax = dmax;
     debug.left = left;
